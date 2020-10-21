f85d40718689d5788f23c4da03bb9ae85ed275cd
Ju Young Kim <juyoung@praelexis.com>
2020-10-21 15:07:06 +0200

    second challenge

---
+++ challenges/easy/alphabetically_maximum_substring.py
@@ -0,0 +1,13 @@
+"""
+Challenege - Find an alphabetically maximum substring from a given string.
+
+E.g.
+
+Given s = "bacb"
+
+Possible substring = ["b", "ba", "bac", "bacb", "a", "ac", "acb", "c", "cb"]
+
+Alphabetically maximum substring is therefore "cb".
+
+Complete a function that returns the maximum substring given a string input.
+"""
